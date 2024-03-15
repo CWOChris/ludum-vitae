9b7665abe898d1ef4404b68f94e0bfc07ade576e
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-15 13:53:09 -0500

    added templates for glider, toad and beacon

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,4 +1,3 @@
-// Assuming generateEmptyGrid is defined in this file or imported from another
 const numRows = 50;
 const numCols = 50;
 
@@ -13,11 +12,29 @@ const generateEmptyGrid = () => {
 export const blankTemplate = generateEmptyGrid();
 
 export const template1 = generateEmptyGrid();
-// Modify template1 to include a specific pattern (e.g., a glider)
+// This is a glider
 template1[1][2] = 1;
 template1[2][3] = 1;
 template1[3][1] = 1;
 template1[3][2] = 1;
 template1[3][3] = 1;
 
-// Define more templates as needed...
+export const template2 = generateEmptyGrid();
+// This is a Beacon
+template2[1][1] = 1;
+template2[1][2] = 1;
+template2[2][1] = 1;
+template2[2][2] = 1;
+template2[3][3] = 1;
+template2[3][4] = 1;
+template2[4][3] = 1;
+template2[4][4] = 1;
+
+export const template3 = generateEmptyGrid();
+// This is a Toad
+template3[2][2] = 1;
+template3[2][3] = 1;
+template3[2][4] = 1;
+template3[3][1] = 1;
+template3[3][2] = 1;
+template3[3][3] = 1;
