f1519133ac568719b6c497238c6c522fea4646b2
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-15 14:01:06 -0500

    corrected errors in HomePage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-// App.tsx adjustments
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GameOfLife from "./GameOfLife";
 import HomePage from "./HomePage";
-import { template1, blankTemplate /*, template2, template3 -- Ensure these are defined */ } from "./Template";
+import { blankTemplate, template1, template2, template3 } from "./Template";
 
-// Create a mapping or use a switch-case to select the template based on a parameter
-const selectTemplate = (templateId) => {
-  switch (templateId) {
-    case '1': return template1;
-    // case '2': return template2;
-    // case '3': return template3;
-    default: return blankTemplate;
-  }
+// A utility function or a similar approach to select the appropriate template
+const getTemplate = (templateId) => {
+    switch(templateId) {
+        case "1":
+            return template1;
+        case "2":
+            return template2;
+        case "3":
+            return template3;
+        default:
+            return blankTemplate;
+    }
 };
 
-const GameOfLifeRouteWrapper = () => {
-  let { template } = useParams();
-  let initialGrid = selectTemplate(template);
-  return <GameOfLife initialGrid={initialGrid} />;
+// A component to wrap GameOfLife and pass the selected template
+const GameOfLifeWrapper = ({ templateId }) => {
+    const initialGrid = getTemplate(templateId);
+    return <GameOfLife initialGrid={initialGrid} />;
 };
 
-const App: React.FC = () => {
+const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/GameOfLife/:template" element={<GameOfLifeRouteWrapper />} />
+                <Route path="/GameOfLife/:templateId" element={({ match }) => <GameOfLifeWrapper templateId={match.params.templateId} />} />
             </Routes>
         </Router>
     );
