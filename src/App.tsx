b47a87526b7d3877837d85f8f5bae0ef6900b958
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-15 13:33:53 -0500

    added routing for templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GameOfLife from "./GameOfLife";
+import HomePage from "./HomePage";
+import { template1, template2, template3, blankTemplate } from "./Template";
 
-function App() {
-  return (
-        <div className="App">
-            <GameOfLife />
-        </div>
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={blankTemplate} />} />
+                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={template1} />} />
+                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={template2} />} />
+                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={template3} />} />
+            </Routes>
+        </Router>
     );
-}
+};
 
 export default App;
