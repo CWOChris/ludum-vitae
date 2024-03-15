f53f027908f2e4d308864b63f758e486884ded7c
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-15 13:46:02 -0500

    changes to App.tsx to allow user to start from a template

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+// App.tsx adjustments
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GameOfLife from "./GameOfLife";
 import HomePage from "./HomePage";
-import { template1, template2, template3, blankTemplate } from "./Template";
+import { template1, blankTemplate /*, template2, template3 -- Ensure these are defined */ } from "./Template";
+
+// Create a mapping or use a switch-case to select the template based on a parameter
+const selectTemplate = (templateId) => {
+  switch (templateId) {
+    case '1': return template1;
+    // case '2': return template2;
+    // case '3': return template3;
+    default: return blankTemplate;
+  }
+};
+
+const GameOfLifeRouteWrapper = () => {
+  let { template } = useParams();
+  let initialGrid = selectTemplate(template);
+  return <GameOfLife initialGrid={initialGrid} />;
+};
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={blankTemplate} />} />
-                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={template1} />} />
-                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={template2} />} />
-                <Route path="/GameOfLife/:template" element={<GameOfLife initialGrid={template3} />} />
+                <Route path="/GameOfLife/:template" element={<GameOfLifeRouteWrapper />} />
             </Routes>
         </Router>
     );
