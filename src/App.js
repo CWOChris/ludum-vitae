5fb6dd4955fe7f972ac774f9b73270f1cb471ab4
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-15 17:06:32 -0500

    changed to HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import GameOfLifeWrapper from './GameOfLifeWrapper'; // Assuming GameOfLifeWrapper is now its own component
 import HomePage from './HomePage';
 
